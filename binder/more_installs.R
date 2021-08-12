4ec20815bd01ee1d0d1e8791f8675a93bff900e5
text-utl <lambamanika07@gmail.com>
2021-08-13 05:26:40 +0530

    Aug 2021

---
+++ binder/more_installs.R
@@ -0,0 +1,4 @@
+# Specifying the mirrors is based on https://stackoverflow.com/a/11488224/8508004 and was added because 
+# without them R complained
+install.packages("textmineR", repos='http://cran.us.r-project.org') 
+install.packages("RTextTools", repos='http://cran.us.r-project.org')
