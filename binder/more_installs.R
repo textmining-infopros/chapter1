6382ab4d14b688eedead637e89ed7d674bdfd082
Text Mining for Information Professionals: An Uncharted Territory <71525827+textmining-infopros@users.noreply.github.com>
2021-08-13 05:46:59 +0530

    Update more_installs.R

--- binder/more_installs.R
+++ binder/more_installs.R
@@ -1,4 +1,4 @@
 # Specifying the mirrors is based on https://stackoverflow.com/a/11488224/8508004 and was added because 
 # without them R complained
-install.packages("textmineR", repos='http://cran.us.r-project.org') 
-install.packages("RTextTools", repos='http://cran.us.r-project.org')
+install.packages("textmineR", repos='https://CRAN.R-project.org/package=textmineR') 
+install.packages("RTextTools", repos='https://CRAN.R-project.org/package=RTextTools')
